20baab912ba01f73e72b2a49c6d8595ae5b666ad
Dref81 <yusufakyuzoglu81@hotmail.com>
2024-02-14 17:07:51 +0300

    Rulet Final

--- Rulet.py
+++ Rulet.py
@@ -1,15 +1,21 @@
 import random
 
 
-renkler = ["Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Yeşil","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı",]
+renkler = ["Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Siyah","Yeşil","Kirmizi","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı","Kırmızı"]
 renk = random.choice(renkler)
-
 print("Çevirelim mi? 'y/n'")
 cevap1 = input()
-if cevap1 == "y"or"Y":    
-    sayi1 = str(random.randrange(0,36))
-    print(sayi1 +","+ renk)
-elif cevap1 == "n" or "N":
+devam = 0
+if cevap1 == "y":
+    devam = 1
+else:
     print("Tamamdır.Hoşçakal")
 
-
+while devam == 1:
+    
+    
+    if cevap1 == "y"or"Y": 
+        sayi1 = str(random.randrange(0,36))
+        print(sayi1 +","+ renk)
+        devam = 0
+        input()    
