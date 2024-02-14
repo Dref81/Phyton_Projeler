f1f7c8157f8b267fa1b000ada2f2d4e22cac1d11
Dref81 <yusufakyuzoglu81@hotmail.com>
2024-02-14 12:27:48 +0300

    deneme_bilmemkaç

--- Rulet.py
+++ Rulet.py
@@ -1,9 +1,13 @@
 import random
 
-def rulet(sayi1):
+def rulet_sayi(sayi1):
     sayi3 = random.randrange(0,sayi1)
     print(sayi3)
 
+def rulet_renk():
+        print()
+
 print("Sayı girin")
 sayi1 = int(input())
-rulet(sayi1)
+rulet_sayi(sayi1)
+
