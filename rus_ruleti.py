0335b495078fd5f52576dfa997e8120aacf8fd7d
Dref81 <yusufakyuzoglu81@hotmail.com>
2024-02-20 20:22:31 +0300

    artık son

--- rus_ruleti.py
+++ rus_ruleti.py
@@ -1,10 +1,11 @@
 import random
 yasam = 2
-shot = random.randrange(1,4)
-fatal_bullet = random.randrange(1,4)
+shot = random.randrange(1,6)
+fatal_bullet = random.randrange(1,6)
 print("Ateşlemek için Enter'a bas")
 input()
 while yasam > 0:
+    shot = random.randrange(1,6)
     if shot == fatal_bullet:
         print("Öldün")
         yasam = 0
