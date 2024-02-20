049f8cfa71c0069e092af61cf242a9a8068597a5
Dref81 <yusufakyuzoglu81@hotmail.com>
2024-02-20 20:08:48 +0300

    rus ruleti bitti

---
+++ rus_ruleti.py
@@ -0,0 +1,15 @@
+import random
+yasam = 2
+shot = random.randrange(1,6)
+fatal_bullet = random.randrange(1,6)
+print("Ateşlemek için Enter'a bas")
+input()
+while yasam > 0:
+    if shot == fatal_bullet:
+        print("Öldün")
+        yasam = 0
+        input()
+    else:
+        yasam = 1
+        print("Yaşadın")
+        input()
